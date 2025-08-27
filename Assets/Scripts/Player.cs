8d661f7947d6362f3f179d0550ddad0e0e9a43b0
GabrielBergoeing <141929892+GabrielBergoeing@users.noreply.github.com>
2025-08-27 14:33:41 -0400

    add: input system created & entity class
    - New Input System created in Input System folder. Still needs to set Player scheme and layouts
    - Entity base class created, handles the core logic of all PC and NPC, like statemachines, animations & colision detection. Will help a lot when creating multiple interactive plants
    - TODO: Base of statemachine, entity_states & player_states.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,47 @@
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : Entity
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public PlayerInputSet input { get; private set; }
+
+    // Here we define variables to store states, like
+    // public Player_IdleState idleState { get; private set; }
+    // public Player_MoveState moveState { get; private set; }
+
+    [Header("Movement variables")]
+    [SerializeField] public Vector2 moveInput { get; private set; }
+    public float moveSpeed = 8;
+
+    protected override void Awake()
     {
-        
+        base.Awake();
+        input = new PlayerInputSet();
+        // sfx = GetComponent<Player_SFX>();
+
+        // idleState = new Player_IdleState(this, stateMachine, "idle");
+        // moveState = new Player_MoveState(this, stateMachine, "move");
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Start()
     {
-        
+        base.Start();
+        //stateMachine.Initialize(idleState);
     }
+
+    private void OnEnable()
+    {
+        input.Enable();
+
+        // Define the Player type control scheme
+        //input.Player.Movement.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        //input.Player.Movement.canceled += ctx => moveInput = Vector2.zero;
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
+    }
+
+    // Teleport player's transform to given position, useful for start of match or outofbounds
+    public void TeleportPlayer(Vector3 position) => transform.position = position;
 }
