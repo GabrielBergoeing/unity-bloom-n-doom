85a3b6fa3f9eacc51fba85881265cdd9f993745e
Gabriel <gbergoeing@miuandes.cl>
2025-10-18 13:07:05 -0300

    add: Main Menu & UI Controls

---
+++ Assets/Scripts/UI/UI_MainMenu.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class UI_MainMenu : MonoBehaviour
+{
+    public void Start()
+    {
+        //Fade Effect, sfx...
+    }
+
+    public void PlayBTN()
+    {
+
+    }
+
+    public void QuitGameBTN()
+    {
+        Application.Quit();
+    }
+}
