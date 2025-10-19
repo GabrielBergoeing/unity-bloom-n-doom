050430d8c1e9e9aba2a30ff9e74e043d25a32453
GabrielBergoeing <141929892+GabrielBergoeing@users.noreply.github.com>
2025-10-19 16:02:35 -0300

    add: plant mechanics
    added timer for plant duration, health and scoring

--- Assets/Scripts/Plants/Plant.cs
+++ Assets/Scripts/Plants/Plant.cs
@@ -1,8 +1,15 @@
 using UnityEngine;
 
+//Para la entrega final habra que hacer la clase planta ser heredado desde Entity
 public class Plant : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
+    [Range(0, 5)][SerializeField] private int score = 1;
+    [Range(0, 20)][SerializeField] private float maxHealth = 10;
+    [Range(0f, 60f)][SerializeField] private float witheringTime = 30f;
+
+    private float health;
+    private float timer;
 
     public void Init(Sprite sprite)
     {
@@ -14,7 +21,31 @@ public class Plant : MonoBehaviour
     }
     private void Awake()
     {
+        health = maxHealth;
+        timer = witheringTime;
+
         if (spriteRenderer == null)
             spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0 || health <= 0)
+            Die();
+    }
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    public void WaterPlant()
+    {
+        timer = witheringTime;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
     }
 }
